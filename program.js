604fef5169e192636de871b56c4fc7825035e761
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-04-25 10:47:08 +0200

    fixes

--- program.js
+++ program.js
@@ -1,10 +1,7 @@
+var os = require('os');
 
-//1. enkodowanie
 process.stdin.setEncoding('utf-8');
-//2. nasłuchiwanie na zdarzenia odczytu
 process.stdin.on('readable', function() {
-	// tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
 	if(input !== null) {
         var instruction = input.toString().trim();
@@ -14,14 +11,35 @@ process.stdin.on('readable', function() {
 		        process.exit();
 		        break;
         	case '/versionNode':
-        		process.stdout.write('Node.js version is : ' + process.version.node + '\n');
+        		process.stdout.write('Node.js version is : ' + process.versions.node + '\n');
         		break;
         	case '/SysLang':
         		process.stdout.write('System language is ' + process.env.lang + '\n');
         		break;
+        	case '/getOSinfo':
+			   getOSinfo();
+			break;
         	default :
         		process.stderr.write('Wrong instruction!\n');
         };
     }    
 });
 
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
+}
